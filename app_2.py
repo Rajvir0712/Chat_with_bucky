f9510beb71176606e8a4f398b6fa7c0ce0878257
Rajvir0712 <150268019+Rajvir0712@users.noreply.github.com>
2024-05-09 11:15:25 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -1,17 +1,3 @@
-import streamlit as st
-from bs4 import BeautifulSoup
-import requests
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from dotenv import load_dotenv
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from PIL import Image
-
 # Load environment variables
 load_dotenv()
 
